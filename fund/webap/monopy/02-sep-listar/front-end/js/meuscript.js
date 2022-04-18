37245e85e1d6219e67097a59ede8eae094f199d6
Hylson <hvescovi@gmail.com>
2022-04-18 07:52:33 -0300

    comentários

--- fund/webap/monopy/02-sep-listar/front-end/js/meuscript.js
+++ fund/webap/monopy/02-sep-listar/front-end/js/meuscript.js
@@ -1,23 +1,25 @@
-$(function() { // quando o documento estiver pronto/carregado
-    
+$(function () { // quando o documento estiver pronto/carregado
+
+    // chamada ao backend
     $.ajax({
         url: 'http://localhost:5000/retornar_pessoas',
         method: 'GET',
         dataType: 'json', // os dados são recebidos no formato json
         success: listar, // chama a função listar para processar o resultado
-        error: function() {
+        error: function () {
             alert("erro ao ler dados, verifique o backend");
         }
     });
 
-    function listar (pessoas) {
+    // função executada quando tudo dá certo
+    function listar(pessoas) {
         // percorrer a lista de pessoas retornadas; 
         for (var i in pessoas) { //i vale a posição no vetor
             lin = '<tr>' + // elabora linha com os dados da pessoa
-              '<td>' + pessoas[i].nome + '</td>' + 
-              '<td>' + pessoas[i].email + '</td>' + 
-              '<td>' + pessoas[i].telefone + '</td>' + 
-              '</tr>';
+                '<td>' + pessoas[i].nome + '</td>' +
+                '<td>' + pessoas[i].email + '</td>' +
+                '<td>' + pessoas[i].telefone + '</td>' +
+                '</tr>';
             // adiciona a linha no corpo da tabela
             $('#corpoTabelaPessoas').append(lin);
         }
