15b5617e0e0a1204a7b195759d0b74b267b675d3
Hylson <hvescovi@gmail.com>
2022-04-28 05:40:03 -0300

    post básico python

---
+++ fund/webap/monopy/03-post/front-end/js/meuscript.js
@@ -0,0 +1,77 @@
+$(function() { // quando o documento estiver pronto/carregado
+    
+    // código para mapear click do botão incluir pessoa
+    $(document).on("click", "#btIncluirPessoa", function() {
+        //pegar dados da tela
+        nome = $("#campoNome").val();
+        email = $("#campoEmail").val();
+        tel = $("#campoTelefone").val();
+        // preparar dados no formato json
+        var dados = JSON.stringify({ nome: nome, email: email, telefone: tel });
+        // fazer requisição para o back-end
+        $.ajax({
+            url: 'http://localhost:5000/incluir_pessoa',
+            type: 'POST',
+            dataType: 'json', // os dados são recebidos no formato json
+            contentType: 'application/json', // tipo dos dados enviados
+            data: dados, // estes são os dados enviados
+            success: pessoaIncluida, // chama a função listar para processar o resultado
+            error: erroAoIncluir
+        });
+        function pessoaIncluida (retorno) {
+            if (retorno.resultado == "ok") { // a operação deu certo?
+                // informar resultado de sucesso
+                alert("Pessoa incluída com sucesso!");
+                $("#mensagem").text("Pessoa incluída com sucesso!");
+                // limpar os campos
+                $("#campoNome").val("");
+                $("#campoEmail").val("");
+                $("#campoTelefone").val("");
+            } else {
+                // informar mensagem de erro
+                alert("ERRO na inclusão: "+retorno.resultado + ":" + retorno.detalhes);
+            }            
+        }
+        function erroAoIncluir (retorno) {
+            // informar mensagem de erro
+            alert("ERRO ao contactar back-end: "+retorno.resultado + ":" + retorno.detalhes);
+        }
+    });
+
+    $(document).on("click", "#btIncluirPessoa2", function() {
+        //pegar dados da tela
+        nome = $("#campoNome").val();
+        email = $("#campoEmail").val();
+        tel = $("#campoTelefone").val();
+        // preparar dados no formato json
+        var dados = JSON.stringify({ nome: nome, email: email, telefone: tel });
+        // fazer requisição para o back-end
+        $.ajax({
+            url: 'http://localhost:5000/incluir_pessoa',
+            type: 'POST',
+            dataType: 'json', // os dados são recebidos no formato json
+            contentType: 'application/json', // tipo dos dados enviados
+            data: dados, // estes são os dados enviados
+            success: pessoaIncluida, // chama a função listar para processar o resultado
+            error: erroAoIncluir
+        });
+        function pessoaIncluida (retorno) {
+            if (retorno.resultado == "ok") { // a operação deu certo?
+                // informar resultado de sucesso
+                alert("Pessoa incluída com sucesso!");
+                // limpar os campos
+                $("#campoNome").val("");
+                $("#campoEmail").val("");
+                $("#campoTelefone").val("");
+            } else {
+                // informar mensagem de erro
+                alert("ERRO na inclusão: "+retorno.resultado + ":" + retorno.detalhes);
+            }            
+        }
+        function erroAoIncluir (retorno) {
+            // informar mensagem de erro
+            alert("ERRO ao contactar back-end: "+retorno.resultado + ":" + retorno.detalhes);
+        }
+    });
+
+});
