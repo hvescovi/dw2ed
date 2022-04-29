357556a6780fb8663c91089ea6cc1e8c1b0f4db7
Hylson <hvescovi@gmail.com>
2022-04-29 16:40:28 -0300

    teste acesso sqlite com alguns jars

---
+++ bd/java/sqlite/README.md
@@ -0,0 +1,3 @@
+sqlite
+
+Este programa realiza acesso a um banco de dados SQLite via JDBC java. É um projeto criado no NetBeans.
---
+++ bd/java/sqlite/src/acesso/TesteAcessoSQLite.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package acesso;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author friend
+ */
+public class TesteAcessoSQLite {
+
+    public static final String URL = "jdbc:sqlite:/home/friend/01-github/dw2ed/bd/java/acesso-sqlite/companies_1000.db";
+
+    public static void main(String[] args) {
+
+        Connection con;
+        Statement st;
+        ResultSet result;
+        try {
+            con = DriverManager.getConnection(URL);
+            st = con.createStatement();
+            result = st.executeQuery("select * from compania");
+            while (result.next()) {
+                for (int i = 1; i <= 11; i++) {
+                    System.out.print(result.getString(i) + " | ");
+                }
+                System.out.println("");
+            }
+        } catch (SQLException ex) {
+            System.out.print("Erro no SQL: " + ex.getMessage());
+        }
+    }
+
+}
