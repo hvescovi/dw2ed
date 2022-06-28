547ae302512ee7104a95b1f9c79af020ed37a2e4
Hylson <hvescovi@gmail.com>
2022-06-28 14:20:59 -0300

    sessão flask server-side

--- fund/webap/monopy/09-sessao-flask/servidor.py
+++ fund/webap/monopy/09-sessao-flask/servidor.py
@@ -1,19 +1,75 @@
-from flask import Flask, session
-from flask_session import Session  # new style
-# from flask.ext.session import Session  # old style
+# https://testdriven.io/blog/flask-server-side-sessions/
+# https://stackoverflow.com/questions/15995919/how-to-use-curl-to-send-cookies/23039038#23039038
+# https://flask.palletsprojects.com/en/2.0.x/config/#SECRET_KEY
+
+from flask import Flask, request, session, jsonify
+from flask_session import Session
 
 app = Flask(__name__)
+
+# a chave secreta é usada para assinar com criptografia os cookies
+# usados para armazenar o identificador da sessão
+app.secret_key = '$#EWFGHJUI*&DEGBHYJU&Y%T#RYJHG%##RU&U'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route('/set/')
+@app.route('/')
+def padrao():
+    return 'backend server-side.'
+    
+@app.route('/set', methods=['POST'])
 def set():
-    session['key'] = 'value'
-    return 'ok'
+     # preparar uma resposta otimista
+    resposta = jsonify({"resultado": "ok", "detalhes": "ok"})
+    # receber as informações
+    dados = request.get_json()  
+    try:  
+        if 'chave' not in dados: # não tem o atributo chave?
+            resposta = jsonify({"resultado": "erro", 
+                            "detalhes": "Atributo chave não encontrado"})
+        else:
+            # armazena na sessão :-)
+            session[dados['chave']] = dados['valor']
+    except Exception as e:  # em caso de erro...
+        # informar mensagem de erro
+        resposta = jsonify({"resultado": "erro", "detalhes": str(e)})
+    # adicionar cabeçalho de liberação de origem
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta  # responder!
 
-@app.route('/get/')
+@app.route('/get')
 def get():
-    return session.get('key', 'not set')
+    try:
+        dados = request.get_json()  
+        retorno = {'resultado': 'ok'}
+        retorno.update({'detalhes': session[dados['chave']]}) # concatenar dois json's (dicionários)
+        resposta = jsonify(retorno)
+    except Exception as e:  # em caso de erro...
+        # informar mensagem de erro
+        resposta = jsonify({"resultado": "erro", "detalhes": str(e)})
+    
+    # adicionar cabeçalho de liberação de origem
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta  # responder!
 
-app.run()
+@app.route('/delete')
+def delete():
+    try:
+        dados = request.get_json()  
+        session.pop(dados['chave'], default=None)
+        resposta = jsonify({"resultado": "ok", "detalhes": "ok"})        
+    except Exception as e:  # em caso de erro...
+        # informar mensagem de erro
+        resposta = jsonify({"resultado": "erro", "detalhes": str(e)})
+            
+    # adicionar cabeçalho de liberação de origem
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta  # responder!
+
+app.run(debug=True)
+
+# teste via curl:
+# curl -c /tmp/cookie -X POST -d '{"chave":"email","valor":"hvescovi@hotmail.com"}' -H "Content-Type:application/json" localhost:5000/set
+# curl -b "/tmp/cookie" -d '{"chave":"email"}' -X GET -H "Content-Type:application/json"  localhost:5000/get
+# curl -b "/tmp/cookie" -d '{"chave":"popo"}' -X GET -H "Content-Type:application/json" localhost:5000/delete
--- fund/webap/monopy/09-sessao-flask/servidor2.py
+++ fund/webap/monopy/09-sessao-flask/servidor2.py
@@ -1,19 +1,19 @@
 # https://testdriven.io/blog/flask-server-side-sessions/
+# https://stackoverflow.com/questions/15995919/how-to-use-curl-to-send-cookies/23039038#23039038
+# https://flask.palletsprojects.com/en/2.0.x/config/#SECRET_KEY
 
 from datetime import timedelta
 
 from flask import Flask, render_template_string, request, session, redirect, url_for
 from flask_session import Session
 
-
-# Create the Flask application
 app = Flask(__name__)
 
-# Details on the Secret Key: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
 # NOTE: The secret key is used to cryptographically-sign the cookies used for storing
 #       the session identifier.
-app.secret_key = 'BAD_SECRET_KEY'
-
+# a chave secreta é usada para assinar com criptografia os cookies
+# usados para armazenar o identificador da sessão
+app.secret_key = 'palavra-secreta-deve-ser-bem-dificil-$#EWFGHJUI*&DEGBHYJU&Y%T'
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
@@ -23,13 +23,18 @@ Session(app)
 # Create and initialize the Flask-Session object AFTER `app` has been configured
 server_session = Session(app)
 
-
+@app.route('/')
+def padrao():
+    return '''Testando sessão client-side.
+    <a href="/set_email">Iniciar</a>'''
+    
 @app.route('/set_email', methods=['GET', 'POST'])
 def set_email():
     if request.method == 'POST':
         # Save the form data to the session object
         session['email'] = request.form['email_address']
-        return redirect(url_for('get_email'))
+        return 'OK, <a href="/get_email">verifique</a> se o valo está gravado na sessão.'
+        # return redirect(url_for('get_email'))
 
     return """
         <form method="post">
@@ -38,7 +43,6 @@ def set_email():
             <button type="submit">Submit</button
         </form>
         """
-
 
 @app.route('/get_email')
 def get_email():
@@ -59,4 +63,8 @@ def delete_email():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+# teste via curl:
+# curl localhost:5000/set_email -X POST -d "email_address=hvescovi@hotmail.com" -c /tmp/cookie
+# curl -b "/tmp/cookie"  localhost:5000/get_email    
