109eda2336f679332001f6c3b1d77c8888a87698
Hylson <hvescovi@gmail.com>
2022-07-04 07:31:01 -0300

    ajuste de servidor web com porta

--- fund/webap/monopy/10-sessao-login-front-back/front-end/js/inicializacao.js
+++ fund/webap/monopy/10-sessao-login-front-back/front-end/js/inicializacao.js
@@ -7,12 +7,18 @@ $(function () { // quando o documento estiver pronto/carregado
     var partes = comeco.split("/"); // quebra a string por barras; no exemplo: ['localhost','principal.html']
     var primeiro = partes[0]; // pega o que tem antes da primeira barra; variável GLOBAL
 
-    alert(primeiro);
-    var parte2 = primeiro.split(":") // quebra por ":", caso tenha, por exemplo: 'localhost:81'
-    meuip = parte2[0] // pega a primeira parte: só o endereço, sem a porta
-    alert(meuip);
+    posicao_doispontos = primeiro.indexOf(":");
+    if (posicao_doispontos >= 0) { // contém dois pontos?
+        meuip = primeiro.substring(0, posicao_doispontos);
+    } else {
+        meuip = primeiro;
+    }
+    var mensagem = "URL = " + url + "<br/>Endereço do servidor: "+meuip;
+    $("#mostrar").html(mensagem);
 
     sessionStorage.setItem("meuip",meuip); // guarda na sessão
 
+    // DESCOMENTAR A LINHA ABAIXO PARA 
+    // fazer o encaminhamento automático ao início do sistema
     //window.location = "principal.html"; // vai para a página principal do sistema
 });
