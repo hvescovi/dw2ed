5cef67e3920f47e9ffcc16da2a66490afe7bca69
Hylson <hvescovi@gmail.com>
2022-07-19 14:24:58 -0300

    testes com curl login e listar

--- fund/webap/monopy/13-login-no-bd/back-end/rotas/listar.py
+++ fund/webap/monopy/13-login-no-bd/back-end/rotas/listar.py
@@ -38,6 +38,34 @@ def listar(classe):
     return resposta
 
     '''
-    exemplo de teste:
-    $ curl -X POST -d '{"login":"josilva@gmail.com"} localhost:5000/listar/ExameRealizado
+    exemplos de testes:
+
+curl -X POST -d '{"login":"josilva@gmail.com"}' localhost:5000/listar/Pessoa
+{
+  "detalhes": "aus\u00eancia de login josilva@gmail.com na sess\u00e3o", 
+  "resultado": "erro"
+}
+
+curl -b /tmp/cookie -X POST -d '{"login":"josilva@gmail.com"}' localhost:5000/listar/Pessoa
+{
+  "detalhes": [
+    {
+      "email": "josilva@gmail.com", 
+      "id": 1, 
+      "nome": "Jo\u00e3o da Silva", 
+      "senha": "53d32ccc72f314a7c2055f76bc938151b3a0b83861c8ec60a05dfd355f456b5c5fafbd0470da1bdefbbf1f9a9b256990e9c69b5faf93f6d79305ea1e4f7db9b5", 
+      "telefone": "47 99012 3232"
+    }, 
+    {
+      "email": "molive@gmail.com", 
+      "id": 2, 
+      "nome": "Maria Oliveira", 
+      "senha": "c31bdc2870e80708a57d0f8577f7d8b08ca4361f0a8528baf71cdcb00086e005b3b2d139d3f0d6192e17f354680c5f915c20a8118d90bec16af818e49ae73da2", 
+      "telefone": "47 98822 2531"
+    }
+  ], 
+  "resultado": "ok"
+}
+ 
+
 '''
