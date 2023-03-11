dcb213a16c2fc6162a66ddb3e180e655ac3d6317
Hylson <hvescovi@gmail.com>
2023-03-11 11:22:19 -0300

    ajustes no demo de pacotes

--- fund/python/pacote/ex4/classes/pessoa.py
+++ fund/python/pacote/ex4/classes/pessoa.py
@@ -1,6 +1,12 @@
 if __name__ == "__main__":
   # configuração para permitir importação de arquivos do diretório superior
   # https://codeolives.com/2020/01/10/python-reference-module-in-parent-directory/
+  # isso é uma adaptação que pode ser usada enquanto estamos aprendendo
+  # daria pra executar o import no próprio programa, sem 
+  # necessitar da chamada por um programa "em nível superior" que conheça
+  # a estrutura
+  # depois de aprender isso, é melhor não usar :-) e usar da forma correta:
+  # o programa de nível superior chamando os de nível subordinado
   import os, sys
   currentdir = os.path.dirname(os.path.realpath(__file__)) # /home/friend/01-github/dw2ed/fund/python/pacote/ex4/classes
   parentdir = os.path.dirname(currentdir) # /home/friend/01-github/dw2ed/fund/python/pacote/ex4
--- fund/python/pacote/ex7/teste/teste_Aplicativo.py
+++ fund/python/pacote/ex7/teste/teste_Aplicativo.py
@@ -1,14 +1,3 @@
-# apenas o teste do aplicativo contém este código
-# a seguir que permite execução isolada do teste
-# (sem precisar ser chamado por outro arquivo)
-if __name__ == "__main__":
-  # configuração para permitir importação de arquivos do diretório superior
-  # https://codeolives.com/2020/01/10/python-reference-module-in-parent-directory/
-  import os, sys
-  currentdir = os.path.dirname(os.path.realpath(__file__)) # /home/friend/01-github/dw2ed/fund/python/pacote/ex5/classes
-  parentdir = os.path.dirname(currentdir) # /home/friend/01-github/dw2ed/fund/python/pacote/ex5
-  sys.path.append(parentdir)
-
 from classes.aplicativo import Aplicativo
 
 def run():
@@ -16,6 +5,3 @@ def run():
   whats = Aplicativo("WhatsApp", "Envie mensagens em texto, fotos e "+\
       "vídeos para seus amigos", "WhatsApp LLC")
   print(whats)
-
-if __name__ == "__main__":
-  run()
