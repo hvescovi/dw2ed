a6e8df6a154a19f8f5097e6a31f9e97dd122d49b
Hylson <hvescovi@gmail.com>
2023-03-14 12:26:36 -0300

    remoção de códigos obscuros para funcionamento de pacotes :-)

--- fund/python/pacote/ex4/testar_pessoa.py
+++ fund/python/pacote/ex4/testar_pessoa.py
@@ -1,5 +1,9 @@
 from classes.pessoa import Pessoa
+#from classes.pessoa import *
+#from classes import pessoa
 
+#joao = pessoa.Pessoa("João", 70)
+#maria = pessoa.Pessoa("Maria", 130)
 joao = Pessoa("João", 70)
 maria = Pessoa("Maria", 130)
 print(joao)
